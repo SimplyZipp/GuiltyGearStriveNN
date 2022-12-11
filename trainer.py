78fe9fa94768f5e8360a7195761bb908371684fc
SimplyZipp <tengel319@gmail.com>
2022-12-11 11:25:52 -0700

    Added memory object and filled get_discounted_rewards

--- trainer.py
+++ trainer.py
@@ -6,6 +6,7 @@ from torch.nn.parameter import Parameter
 import torch.nn.functional as F
 from torch.distributions import Categorical
 import torch.optim as optim
+from memory import Memory
 
 #here is the code to ddetect HP values and RISC values
 from pymem import *
@@ -36,14 +37,14 @@ def getcurrentstats():
     p1HP = pm.read_int(GetPtrAddr(pm.base_address + 0x4EC5F38, offsets = [0x130, 0x8, 0x2B0, 0x680, 0x1170]))
     p1RISC = pm.read_int(GetPtrAddr(pm.base_address + 0x4EC5F38, offsets = [0x130, 0x188, 0x2A0, 0x6D8, 0xC754]))
     p2RISC = pm.read_int(GetPtrAddr(pm.base_address + 0x4EC5F38, offsets = [0x130, 0x188, 0xC754]))
-    #print("healph P1:" + str(p1HP))
+    #print("health P1:" + str(p1HP))
     #print("health P2: " + str(p2HP))
     #print("RISC P1: " + str(p1RISC))
     #print("RISC P2: " + str(p2RISC))
     return p1HP, p2HP, p1RISC, p2RISC
 
 def getReward(prev_p1HP, prev_p2HP, prev_p1RISC, prev_p2RISC, p1HP, p2HP, p1RISC, p2RISC, position):
-    if(position = "Player 1"): #change to whatever indicator we use to say whether the AI is player 1 or two
+    if(position == "Player 1"): #change to whatever indicator we use to say whether the AI is player 1 or two
         mult = 1
     else:
         mult = -1
@@ -53,7 +54,7 @@ def getReward(prev_p1HP, prev_p2HP, prev_p1RISC, prev_p2RISC, p1HP, p2HP, p1RISC
     Reward += 20 * (p2HP == 0) * mult #+ 20 if you win a round, -20 if you lose
     
     Reward += (prev_p1HP - p1HP)/ 42 * mult 
-    Reward -= (prev_p2HP - p2hp) / 42 * mult #+1 point for every 10% damage dealt, max HP is 420
+    Reward -= (prev_p2HP - p2HP) / 42 * mult #+1 point for every 10% damage dealt, max HP is 420
     
     Reward -= max(p1RISC - prev_p1RISC, 0) / 12800 * mult #+1 point per bar of enemy RISC filled. Max RISC value is 12800.  
     Reward += max(p2RISC - prev_p2RISC, 0) /12800 * mult #if RISC goes below zero, then the enemy is being combo'd and not part of this case
@@ -64,8 +65,8 @@ def getReward(prev_p1HP, prev_p2HP, prev_p1RISC, prev_p2RISC, p1HP, p2HP, p1RISC
     Reward -= (p1RISC > prev_p1RISC) * (prev_p1RISC == 0)/4 
     Reward += (p2RISC > prev_p2RISC) * (prev_p2RISC == 0)/4 #+.25 for winning neutral or okizeme
     
-    Reward += (prev_p1RISC - p1RISC) * (prev_p1HP == p1HP) * (abs(prev_p1RISC - p1RISC) < 1500)) /12800 #gets some reward back for allowing RISC to lower without taking damage
-    Reward -= (prev_p2RISC - p2RISC) * (prev_p2HP == p2HP) * (abs(prev_p2RISC - p2RISC) < 1500)) /12800 #if the RISC drops too much, then you likely just got back up after a combo, so no reward should be given
+    Reward += ( (prev_p1RISC - p1RISC) * (prev_p1HP == p1HP) * (abs(prev_p1RISC - p1RISC) < 1500)) /12800 #gets some reward back for allowing RISC to lower without taking damage
+    Reward -= ( (prev_p2RISC - p2RISC) * (prev_p2HP == p2HP) * (abs(prev_p2RISC - p2RISC) < 1500)) /12800 #if the RISC drops too much, then you likely just got back up after a combo, so no reward should be given
     
     #the expected reward should realistically stay between negative 20 and 20
     return Reward
@@ -74,30 +75,57 @@ def getReward(prev_p1HP, prev_p2HP, prev_p1RISC, prev_p2RISC, p1HP, p2HP, p1RISC
 
 
 class Trainer:
-    def __init__(self, lr):
+    def __init__(self, lr=1e-3, gamma=0.99, save_every=10):
         self.lr = lr
+        self.gamma = gamma
 
-    def train(self, model, logs):
+        self.save_every = save_every
+        self.iterations = 0
+
+    def train(self, model, memory):
         """
         Train the model using the game logs.
         The object logs is not created yet, but it should be an object containing information about how the game
         went, including states, actions, rewards, entropy, log_probs, etc.
         :param model:
-        :param logs:
+        :param memory:
         :return:
         """
+        # Should actually create and agent object and store the optimizer with the model
+        optimizer = optim.Adam(model.parameters(), lr=self.lr)
+
+        loss = self.calc_A2C_loss(memory)
+        loss.backward()
+
+        optimizer.step()
+
+        # Reset memory for next game
+        memory.reset()
+
+        if self.iterations % self.save_every == 0:
+            # TODO: Save model
+            self.iterations = 0
+            pass
+        self.iterations += 1
+
         pass
 
-    def calc_A2C_loss(self):
+    def calc_A2C_loss(self, memory):
         """
         Calculate the total loss for A2C. Not sure what it needs yet
         :return:
         """
         pass
 
-    def get_discounted_rewards(self):
+    def get_discounted_rewards(self, memory):
         """
         Calculate the future discounted rewards from the current state
         :return:
         """
-        pass
+        rewards = torch.zeros(len(memory))
+        val = 0
+        for i in reversed(range(len(memory))):
+            _, _, value, reward = memory[i]
+            val = self.gamma*val + reward
+            rewards[i] = val
+        return rewards
