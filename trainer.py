13f7e62300cc37036f919ebb720727775723a669
Hylialee <105137404+Hylialee@users.noreply.github.com>
2022-12-10 20:39:37 -0700

    Update trainer.py
    adding in the memory grabber for HP and tension, we need to figure out how to store the previously read HP and RISC

--- trainer.py
+++ trainer.py
@@ -7,6 +7,43 @@ import torch.nn.functional as F
 from torch.distributions import Categorical
 import torch.optim as optim
 
+#here is the code to ddetect HP values and RISC values
+from pymem import *
+from pymem.process import *
+import time
+
+#Guilty gear preferred memory space 0x7FF7B92B0000
+#base offset for health 2 0x4EC5F38
+#offsets for health p2 are [0x130, 0x188, 0x6D8, 0x680, 0x1170]
+
+
+pm = Pymem('GGST-Win64-Shipping.exe')
+
+#from stack overflow https://stackoverflow.com/questions/74348681/overflow-while-using-readwritememory
+#this follows the memory addresses the current one. Used to find the memory location of HP and RISC
+def GetPtrAddr(base, offsets):
+    addr = pm.read_longlong(base)
+    for i in offsets:
+        if i != offsets[-1]:
+            addr = pm.read_longlong(addr + i)
+    return addr + offsets[-1]
+
+#good for testing to see if the code works
+#the below offsets and base addresses may not work on different system. Make sure it doesn't break the code if it can't detect a value (i.e. if you're in the main menu)
+'''while 1:
+    #these are the values at the current time t, unless it works differently on different systems, don't worry about changing
+    p2HP = pm.read_int(GetPtrAddr(pm.base_address + 0x4EC5F38, offsets=[0x130, 0x188, 0x6D8, 0x680, 0x1170])) 
+    p1HP = pm.read_int(GetPtrAddr(pm.base_address + 0x4EC5F38, offsets = [0x130, 0x8, 0x2B0, 0x680, 0x1170]))
+    p1RISC = pm.read_int(GetPtrAddr(pm.base_address + 0x4EC5F38, offsets = [0x130, 0x188, 0x2A0, 0x6D8, 0xC754]))
+    p2RISC = pm.read_int(GetPtrAddr(pm.base_address + 0x4EC5F38, offsets = [0x130, 0x188, 0xC754]))
+    print("healph P1:" + str(p1HP))
+    print("health P2: " + str(p2HP))
+    print("RISC P1: " + str(p1RISC))
+    print("RISC P2: " + str(p2RISC))
+    time.sleep(2)'''
+
+
+
 class Trainer:
     def __init__(self, lr):
         self.lr = lr
