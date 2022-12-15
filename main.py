048c73cf55eff55ec1e8fedf7225f38bb9efd4e7
SimplyZipp <tengel319@gmail.com>
2022-12-15 12:14:59 -0700

    Implemented mainv2

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import trainer
 import memory
 import utils
 from threading import Thread
+import time
 
 
 def main():
-    device = 'cpu'
+    device = torch.device('cpu')
     dtype = torch.float32
     if torch.cuda.is_available():
         print('CUDA available')
-        device = 'cuda'
+        device = torch.device('cuda')
     else:
         print('CUDA unavailable')
 
@@ -52,11 +53,11 @@ def main():
         learner.train(mem)
 
 def mainv2():
-    device = 'cpu'
+    device = torch.device('cpu')
     dtype = torch.float32
     if torch.cuda.is_available():
         print('CUDA available')
-        device = 'cuda'
+        device = torch.device('cuda')
     else:
         print('CUDA unavailable')
 
@@ -65,40 +66,62 @@ def mainv2():
 
     # TODO: Load or create new model
     model = modules.A2C(size, nstack, (15, 2), actor_hidden_size=256, device=device, dtype=dtype)
+    model.to(device=device, dtype=dtype)
 
     learner = trainer.Trainer(model, device=device, dtype=dtype)
     mem = memory.Memory()
 
     timer = utils.SimpleTimer(0.1)
     screen = utils.FrameGrabber(size=size)
+    game = utils.GameManager()
 
     while True:
         # Training loop
         past_frames = torch.zeros((nstack, *size))
 
-        start_game()  # TODO
+        print("Starting game...")
+
+        # TODO: start_game()
+        game.initloop(18, 'cpu', 18)  # No idea which character. Also, does this start the game?
+
+        # Wait for game to start
+        started = False
+        while not started:
+            img = screen.color_img()
+            started = game.MatchStart(img)
 
         total_entropy = 0
+        prev_actions = (14, 14)
+        prev_stats = game.getcurrentstats()
+
+        print('Game started, playing game')
         timer.start()
-        while game_running:  # TODO: Get this from final reward?
+        while not game.MatchEnd():
             # Game loop
             past_frames[0] = screen.frame()
             actions, log_prob, entropy, value = model.get_action(past_frames)
 
-            t = Thread(target=send_actions, args=actions)
+            t = Thread(target=game.action, args=[*prev_actions, *actions])  # Might need to pass self
             t.start()
 
             past_frames = torch.roll(past_frames, 1, dims=0)
             total_entropy += entropy
 
-            reward = get_reward()  # TODO
+            reward, prev_stats = game.get_reward(prev_stats, "Player 1")
             mem.add(actions, log_prob, value, reward)
             timer.wait_and_continue()
 
+        print('Game finished, beginning training')
+        start_time = time.time()
+
+        mem.shift_rewards_left()
         mem.entropy = total_entropy
         learner.train(mem)
+
+        end_time = time.time()
+        print(f'Training iteration finished. Elapsed time: {end_time-start_time}')
 
 
 
 if __name__ == '__main__':
-    main()
+    mainv2()
--- utils.py
+++ utils.py
@@ -54,7 +54,12 @@ class FrameGrabber:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = self.transform(image=img)['image']
         return img
-    
+
+    def color_img(self):
+        sct_img = self.sct.grab(self.bounds)
+        img = np.array(sct_img)
+        # Might need to convert from BGR to RGB
+        return img
     
     
     
@@ -229,7 +234,10 @@ class GameManager:
                 self.dpad(5)
                 time.sleep(.5)
         top = np.array([19, 16, 11, 7, 4, 0, 1, 2, 8, 14, 17])
+
+        # Bottom is not used?
         bottom = np.array([18, 13, 10, 6, 3, 33, 5, 9, 12, 15])  # characters located on teh top and bottom
+
         if min((charanum - top) ** 2) == 0:  # determines which row the pointer is on
             row = 1
         else:
@@ -414,6 +422,56 @@ class GameManager:
         if new_button == 13:
             self.button("dash")
         sleep(frame)
+
+    def get_reward(self, prev_stats, position: str):
+        stats = self.getcurrentstats()
+        return self._getReward(*prev_stats, *stats, position), stats
+
+    def getcurrentstats(self):
+        pm = self.pm
+        # these are the values at the current time t, unless it works differently on different systems, don't worry about changing
+        p2HP = pm.read_int(self.GetPtrAddr(pm.base_address + 0x0505E6B8, offsets=[0x138, 0x8, 0x6E0, 0x688, 0x1178]))
+        p1HP = pm.read_int(self.GetPtrAddr(pm.base_address + 0x0505E6B8, offsets=[0x138, 0x0, 0x6E0, 0x688, 0x1178]))
+        p1RISC = pm.read_int(self.GetPtrAddr(pm.base_address + 0x0505E6B8, offsets=[0x130, 0x8, 0x688, 0xC764]))
+        p2RISC = pm.read_int(self.GetPtrAddr(pm.base_address + 0x0505E6B8, offsets=[0x138, 0x8, 0x2A8, 0xC764]))
+        # print("health P1:" + str(p1HP))
+        # print("health P2: " + str(p2HP))
+        # print("RISC P1: " + str(p1RISC))
+        # print("RISC P2: " + str(p2RISC))
+        return p1HP, p2HP, p1RISC, p2RISC
+
+    def _getReward(self, prev_p1HP, prev_p2HP, prev_p1RISC, prev_p2RISC, p1HP, p2HP, p1RISC, p2RISC, position):
+        if (position == "Player 1"):  # change to whatever indicator we use to say whether the AI is player 1 or two
+            mult = 1
+        else:
+            mult = -1
+        Reward = 0
+        # current reward function, can adjust values and add more cases to help improve the behavior of the actor
+        Reward -= 20 * (p1HP == 0) * mult
+        Reward += 20 * (p2HP == 0) * mult  # + 20 if you win a round, -20 if you lose
+
+        Reward += (prev_p1HP - p1HP) / 42 * mult
+        Reward -= (prev_p2HP - p2HP) / 42 * mult  # +1 point for every 10% damage dealt, max HP is 420
+
+        Reward -= max(p1RISC - prev_p1RISC,
+                      0) / 12800 * mult  # +1 point per bar of enemy RISC filled. Max RISC value is 12800.
+        Reward += max(p2RISC - prev_p2RISC,
+                      0) / 12800 * mult  # if RISC goes below zero, then the enemy is being combo'd and not part of this case
+
+        Reward -= 2 * (p1RISC == 12800) * mult
+        Reward += 2 * (
+                    p2RISC == 12800) * mult  # +2 if enemies RISC is full.if the RISC bar is full, really bad things can happen
+
+        Reward -= (p1RISC > prev_p1RISC) * (prev_p1RISC == 0) / 4
+        Reward += (p2RISC > prev_p2RISC) * (prev_p2RISC == 0) / 4  # +.25 for winning neutral or okizeme
+
+        Reward += ((prev_p1RISC - p1RISC) * (prev_p1HP == p1HP) * (
+                    abs(prev_p1RISC - p1RISC) < 1500)) / 12800  # gets some reward back for allowing RISC to lower without taking damage
+        Reward -= ((prev_p2RISC - p2RISC) * (prev_p2HP == p2HP) * (
+                    abs(prev_p2RISC - p2RISC) < 1500)) / 12800  # if the RISC drops too much, then you likely just got back up after a combo, so no reward should be given
+
+        # the expected reward should realistically stay between negative 20 and 20
+        return Reward
 
 
 # startpixels = ((771, 405), (429, 540), (421, 745), (444, 452), (366, 277), (531, 317), (1320, 416), (1300, 639), (953, 285), (906, 599), (1102, 711), (1404, 674), (379, 303), (1025, 184), (888, 693), (1129, 698), (440, 434), (317, 226), (1539, 216), (1606, 855), (1240, 724))
